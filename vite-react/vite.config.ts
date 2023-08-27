65f100cc31839c687f5b05c3b6968dc49335b1ce
seanmcquaid <sean.m.mcquaid@gmail.com>
2023-08-27 18:00:25 -0400

    added shadcn component

--- vite-react/vite.config.ts
+++ vite-react/vite.config.ts
@@ -1,6 +1,7 @@
 /// <reference types="vitest" />
 /// <reference types="vite/client" />
 
+import path from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
@@ -9,43 +10,46 @@ import { visualizer } from 'rollup-plugin-visualizer';
 import generouted from '@generouted/react-router/plugin';
 
 // https://vitejs.dev/config/
-export default defineConfig(() => {
-  return {
-    plugins: [
-      react(),
-      svgr(),
-      checker({ typescript: true }),
-      visualizer(),
-      generouted(),
-    ],
-    preview: {
-      port: 3000,
+export default defineConfig({
+  plugins: [
+    react(),
+    svgr(),
+    checker({ typescript: true }),
+    visualizer(),
+    generouted(),
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
     },
-    server: {
-      port: 3000,
+  },
+  preview: {
+    port: 3000,
+  },
+  server: {
+    port: 3000,
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['./src/setupTests.ts'],
+    exclude: ['e2e', 'node_modules'],
+    coverage: {
+      provider: 'istanbul',
+      reporter: ['lcov'],
+      all: true,
+      include: ['src/**/*.ts', 'src/**/*.tsx'],
+      exclude: [
+        'src/setupTests.ts',
+        'src/testUtils',
+        'src/i18n',
+        'src/main.tsx',
+        'src/env.ts',
+        'src/types',
+        'src/router.ts',
+        'src/pages/_app.tsx',
+        'src/pages/404.tsx',
+      ],
     },
-    test: {
-      globals: true,
-      environment: 'jsdom',
-      setupFiles: ['./src/setupTests.ts'],
-      exclude: ['e2e', 'node_modules'],
-      coverage: {
-        provider: 'istanbul',
-        reporter: ['lcov'],
-        all: true,
-        include: ['src/**/*.ts', 'src/**/*.tsx'],
-        exclude: [
-          'src/setupTests.ts',
-          'src/testUtils',
-          'src/i18n',
-          'src/main.tsx',
-          'src/env.ts',
-          'src/types',
-          'src/router.ts',
-          'src/pages/_app.tsx',
-          'src/pages/404.tsx',
-        ],
-      },
-    },
-  };
+  },
 });
