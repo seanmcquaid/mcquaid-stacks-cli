34b8b65887c3e4bc5a72d62d752291ab3a172fd7
seanmcquaid <sean.m.mcquaid@gmail.com>
2023-09-02 23:19:05 -0400

    remove playwright

--- vite-react/vite.config.ts
+++ vite-react/vite.config.ts
@@ -6,18 +6,11 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 import checker from 'vite-plugin-checker';
-import { visualizer } from 'rollup-plugin-visualizer';
 import generouted from '@generouted/react-router/plugin';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    svgr(),
-    checker({ typescript: true }),
-    visualizer(),
-    generouted(),
-  ],
+  plugins: [react(), svgr(), checker({ typescript: true }), generouted()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
