802810c43d2de6e9d0f349a31684b09d1824d9c5
seanmcquaid <sean.m.mcquaid@gmail.com>
2023-09-14 09:46:04 -0400

    trying

--- vite-react/vite.config.ts
+++ vite-react/vite.config.ts
@@ -14,7 +14,6 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
-      '@/components': path.resolve(__dirname, './src/components'),
     },
   },
   preview: {
