41ddcbe0bb91ada69bd63634c6103bb0a3f84c0e
seanmcquaid <sean.m.mcquaid@gmail.com>
2023-09-14 09:38:49 -0400

    attempt to fix alias

--- vite-react/vite.config.ts
+++ vite-react/vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
+      '@/components': path.resolve(__dirname, './src/components'),
     },
   },
   preview: {
