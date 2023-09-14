e829a1b5ce2dda4c4d2031080a8523992ace87af
seanmcquaid <sean.m.mcquaid@gmail.com>
2023-09-13 22:45:41 -0400

    close modal

--- vite-react/src/pages/react-router-generouted/index.tsx
+++ vite-react/src/pages/react-router-generouted/index.tsx
@@ -39,7 +39,8 @@ const ReactRouterGeneroutedPage = () => {
   };
 
   const handleCloseModal = () => {
-    setSearchParams({});
+    searchParams.delete('modal');
+    setSearchParams(searchParams);
   };
 
   return (
