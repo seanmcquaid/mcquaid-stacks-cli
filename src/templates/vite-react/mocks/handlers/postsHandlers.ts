493065ba5aa3a294c8ceb81d42be56fae6ee3421
seanmcquaid <sean.m.mcquaid@gmail.com>
2023-09-19 17:03:12 -0400

    id

--- src/templates/vite-react/mocks/handlers/postsHandlers.ts
+++ src/templates/vite-react/mocks/handlers/postsHandlers.ts
@@ -14,7 +14,7 @@ export const getPostsHandler = rest.get(
 );
 
 export const getPostByIdHandler = rest.get(
-  'https://jsonplaceholder.typicode.com/posts/1',
+  'https://jsonplaceholder.typicode.com/posts/:id',
   (_, res, context) => {
     return res(context.status(200), context.json(generateMock(postSchema)));
   },
