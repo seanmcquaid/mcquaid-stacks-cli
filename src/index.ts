f3fc7778ee47b1509054acdfc4184f63ea3a68ad
seanmcquaid <sean.m.mcquaid@gmail.com>
2023-09-18 23:42:06 -0400

    cli

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 
-import * as inquirer from "@inquirer/prompts";
-import * as fs from "fs";
-import * as path from "path";
-import * as shell from "shelljs";
-import * as ejs from "ejs";
+import * as inquirer from '@inquirer/prompts';
+import * as fs from 'fs';
+import * as path from 'path';
+import * as shell from 'shelljs';
+import * as ejs from 'ejs';
 
 export interface TemplateData {
   projectName: string;
@@ -14,7 +14,7 @@ export function render(content: string, data: TemplateData) {
   return ejs.render(content, data);
 }
 
-const CHOICES = fs.readdirSync(path.join(__dirname, "templates"));
+const CHOICES = fs.readdirSync(path.join(__dirname, 'templates'));
 
 const CURR_DIR = process.cwd();
 
@@ -32,7 +32,7 @@ export interface CliOptions {
 }
 
 function getTemplateConfig(templatePath: string): TemplateConfig {
-  const configPath = path.join(templatePath, ".template.json");
+  const configPath = path.join(templatePath, '.template.json');
 
   if (!fs.existsSync(configPath)) return {};
 
@@ -55,16 +55,16 @@ function createProject(projectPath: string) {
   return true;
 }
 
-const SKIP_FILES = ["node_modules", ".template.json"];
+const SKIP_FILES = ['node_modules', '.template.json'];
 
 function createDirectoryContents(
   templatePath: string,
   projectName: string,
-  config: TemplateConfig
+  config: TemplateConfig,
 ) {
   const filesToCreate = fs.readdirSync(templatePath);
 
-  filesToCreate.forEach((file) => {
+  filesToCreate.forEach(file => {
     const origFilePath = path.join(templatePath, file);
 
     // get stats about the current file
@@ -73,12 +73,12 @@ function createDirectoryContents(
     if (SKIP_FILES.indexOf(file) > -1) return;
 
     if (stats.isFile()) {
-      let contents = fs.readFileSync(origFilePath, "utf8");
+      let contents = fs.readFileSync(origFilePath, 'utf8');
 
       contents = render(contents, { projectName });
 
       const writePath = path.join(CURR_DIR, projectName, file);
-      fs.writeFileSync(writePath, contents, "utf8");
+      fs.writeFileSync(writePath, contents, 'utf8');
     } else if (stats.isDirectory()) {
       fs.mkdirSync(path.join(CURR_DIR, projectName, file));
 
@@ -86,32 +86,46 @@ function createDirectoryContents(
       createDirectoryContents(
         path.join(templatePath, file),
         path.join(projectName, file),
-        config
+        config,
       );
     }
   });
 }
 
+function showMessage(options: CliOptions) {
+  console.log('');
+  console.log('Done.');
+  console.log(`Go into the project: cd ${options.projectName}`);
+
+  const message = options.config.postMessage;
+
+  if (message) {
+    console.log('');
+    console.log(message);
+    console.log('');
+  }
+}
+
 const promptUser = async () => {
   const answers = {
     template: await inquirer.select({
-      message: "What project template would you like to generate?",
-      choices: CHOICES.map((choice) => ({
+      message: 'What project template would you like to generate?',
+      choices: CHOICES.map(choice => ({
         name: choice,
         value: choice,
       })),
     }),
     name: await inquirer.input({
-      message: "What is your project name?",
+      message: 'What is your project name?',
       validate: (input: string) => {
         if (/^([A-Za-z\-\_\d])+$/.test(input)) return true;
         else
-          return "Project name may only include letters, numbers, underscores and hashes.";
+          return 'Project name may only include letters, numbers, underscores and hashes.';
       },
     }),
   };
 
-  const templatePath = path.join(__dirname, "templates", answers.template);
+  const templatePath = path.join(__dirname, 'templates', answers.template);
   const targetPath = path.join(CURR_DIR, answers.name);
   const templateConfig = getTemplateConfig(templatePath);
 
@@ -133,17 +147,3 @@ const promptUser = async () => {
 };
 
 promptUser();
-
-function showMessage(options: CliOptions) {
-  console.log("");
-  console.log("Done.");
-  console.log(`Go into the project: cd ${options.projectName}`);
-
-  const message = options.config.postMessage;
-
-  if (message) {
-    console.log("");
-    console.log(message);
-    console.log("");
-  }
-}
