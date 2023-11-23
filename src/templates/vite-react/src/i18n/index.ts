e9087673b0a80b9b0f47a86209b6a539e845bbfe
seanmcquaid <sean.m.mcquaid@gmail.com>
2023-11-22 19:57:28 -0500

    domain detection

--- src/templates/vite-react/src/i18n/index.ts
+++ src/templates/vite-react/src/i18n/index.ts
@@ -3,19 +3,44 @@ import { initReactI18next } from 'react-i18next';
 import I18nextBrowserLanguageDetector from 'i18next-browser-languagedetector';
 import locales from './locales';
 
+const languageDetector = new I18nextBrowserLanguageDetector();
+
+languageDetector.addDetector({
+  name: 'domain',
+
+  lookup() {
+    const host = window.location.host;
+    if (host.includes('.ca')) {
+      return 'en-CA';
+    }
+    return 'en-US';
+  },
+
+  cacheUserLanguage(lng) {
+    localStorage.setItem('i18nextLng', lng);
+  },
+});
+
 i18n
   .use(initReactI18next)
-  .use(I18nextBrowserLanguageDetector)
+  .use(languageDetector)
   .init({
+    detection: {
+      order: [
+        'domain',
+        'querystring',
+        'cookie',
+        'localStorage',
+        'navigator',
+        'htmlTag',
+      ],
+    },
     debug: true,
     fallbackLng: 'en-US',
     load: 'currentOnly',
     keySeparator: '.',
     saveMissing: true,
     resources: locales,
-    detection: {
-      lookupFromSubdomainIndex: 2,
-    },
     missingKeyHandler: (lng, ns, key, fallbackValue) => {
       console.warn('Missing Translation Key', lng, ns, key, fallbackValue);
     },
