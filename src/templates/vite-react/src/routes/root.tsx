4bb1897bb6647a2585cb4522f1ab0a7c7de19d3d
seanmcquaid <sean.m.mcquaid@gmail.com>
2023-11-24 21:46:37 -0500

    starting sketching

---
+++ src/templates/vite-react/src/routes/root.tsx
@@ -0,0 +1,17 @@
+import { Outlet } from 'react-router-dom';
+import { ErrorBoundary } from 'react-error-boundary';
+import { Toaster } from 'react-hot-toast';
+import PageError from '@/components/app/PageError';
+
+const Root = () => {
+  return (
+    <ErrorBoundary
+      fallback={<PageError errorText="Sorry, something happened" />}
+    >
+      <Outlet />
+      <Toaster position="bottom-center" />
+    </ErrorBoundary>
+  );
+};
+
+export default Root;
