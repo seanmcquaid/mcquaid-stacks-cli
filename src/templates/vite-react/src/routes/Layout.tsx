f7203fa900b1fdd5918fff5a81a2bf958c1098f7
seanmcquaid <sean.m.mcquaid@gmail.com>
2023-12-28 17:43:51 -0500

    fix casing

--- src/templates/vite-react/src/routes/Layout.tsx
+++ src/templates/vite-react/src/routes/Layout.tsx
@@ -1,7 +1,7 @@
 import { Outlet } from 'react-router-dom';
 import { ErrorBoundary } from 'react-error-boundary';
 import PageError from '@/components/app/PageError';
-import { Toaster } from '@/components/ui/ToasterComp';
+import { Toaster } from '@/components/ui/Toaster';
 
 const Layout = () => {
   return (
