68496c85a537982af13ce10bc005b913842b2125
seanmcquaid <sean.m.mcquaid@gmail.com>
2024-01-04 20:41:55 -0500

    working on more tanstack fun

---
+++ src/templates/vite-react-tanstack-router/src/components/app/NotFound.tsx
@@ -0,0 +1,23 @@
+import { Button } from '@/components/ui/Button';
+import PageWrapper from '@/components/app/PageWrapper';
+import useAppTranslation from '@/hooks/useAppTranslation';
+import { useNavigate } from '@tanstack/react-router';
+
+const NotFound = () => {
+  const { t } = useAppTranslation();
+  const navigate = useNavigate();
+
+  const handleOnClick = () => {
+    navigate({ to: t('Routes.home') });
+  };
+
+  return (
+    <PageWrapper>
+      <h1>{t('NotFoundPage.notFound')}</h1>
+      <p>{t('NotFoundPage.pleaseTryADifferentRoute')}</p>
+      <Button onClick={handleOnClick}>{t('NotFoundPage.home')}</Button>
+    </PageWrapper>
+  );
+};
+
+export default NotFound;
--- src/templates/vite-react-tanstack-router/src/routeTree.gen.ts
+++ src/templates/vite-react-tanstack-router/src/routeTree.gen.ts
@@ -1,9 +1,13 @@
 import { Route as rootRoute } from "./routes/__root"
 import { Route as IndexRoute } from "./routes/index"
+import { Route as ReactHookFormZodIndexRoute } from "./routes/react-hook-form-zod/index"
 
 declare module "@tanstack/react-router" {
   interface FileRoutesByPath {
     "/": {
+      parentRoute: typeof rootRoute
+    }
+    "/react-hook-form-zod/": {
       parentRoute: typeof rootRoute
     }
   }
@@ -14,4 +18,12 @@ Object.assign(IndexRoute.options, {
   getParentRoute: () => rootRoute,
 })
 
-export const routeTree = rootRoute.addChildren([IndexRoute])
+Object.assign(ReactHookFormZodIndexRoute.options, {
+  path: "/react-hook-form-zod/",
+  getParentRoute: () => rootRoute,
+})
+
+export const routeTree = rootRoute.addChildren([
+  IndexRoute,
+  ReactHookFormZodIndexRoute,
+])
