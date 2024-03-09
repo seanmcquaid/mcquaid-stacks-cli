6de8de8777168b27353819ffa264ddc1e8889ff3
seanmcquaid <sean.m.mcquaid@gmail.com>
2024-03-08 21:55:07 -0500

    layout

--- src/templates/vite-remix-ssr/app/root.tsx
+++ src/templates/vite-remix-ssr/app/root.tsx
@@ -9,9 +9,11 @@ import {
   useNavigation,
   json,
   useLoaderData,
+  ScrollRestoration,
 } from '@remix-run/react';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { QueryClientProvider } from '@tanstack/react-query';
+import type { PropsWithChildren } from 'react';
 import { useEffect, useState } from 'react';
 import type { LoaderFunctionArgs } from '@remix-run/node';
 import { useChangeLanguage } from 'remix-i18next/react';
@@ -29,84 +31,9 @@ export async function loader({ request }: LoaderFunctionArgs) {
   return json({ locale });
 }
 
-export function ErrorBoundary() {
-  const error = useRouteError();
-  const { i18n } = useAppTranslation();
-
-  useEffect(() => {
-    // Log to service of some sort
-    console.error(error);
-  }, [error]);
-
-  return (
-    <html lang={i18n.language}>
-      <head>
-        <meta charSet="UTF-8" />
-        <meta name="description" content="Vite App" />
-        <meta name="viewport" content="width=device-width,initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-        <meta name="theme-color" content="#ffffff" />
-        <meta
-          httpEquiv="Cache-Control"
-          content="no-cache, no-store, must-revalidate"
-        />
-        <meta httpEquiv="Pragma" content="no-cache" />
-        <meta httpEquiv="Expires" content="0" />
-        <title>{'<%= projectName %>'}</title>
-        <Meta />
-        <Links />
-      </head>
-      <body>
-        <PageError errorText="There was an app crash!" />
-        <Scripts />
-      </body>
-    </html>
-  );
-}
-
-export function HydrateFallback() {
-  const [isLoading, setIsLoading] = useState(false);
-
-  useEffect(() => {
-    setIsLoading(true);
-  }, []);
-
-  return (
-    // eslint-disable-next-line jsx-a11y/html-has-lang
-    <html className="h-screen w-full">
-      <head>
-        <meta charSet="UTF-8" />
-        <meta name="description" content="Vite App" />
-        <meta name="viewport" content="width=device-width,initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-        <meta name="theme-color" content="#ffffff" />
-        <meta
-          httpEquiv="Cache-Control"
-          content="no-cache, no-store, must-revalidate"
-        />
-        <meta httpEquiv="Pragma" content="no-cache" />
-        <meta httpEquiv="Expires" content="0" />
-        <title>{'<%= projectName %>'}</title>
-        <Meta />
-        <Links />
-      </head>
-      <body>
-        <LoadingOverlay isLoading={isLoading} />
-        <Scripts />
-        <noscript>
-          Your browser does not support JavaScript or it is not enabled! Please
-          re-enable JavaScript in order to use this site.
-        </noscript>
-      </body>
-    </html>
-  );
-}
-
-const Root = () => {
+export function Layout({ children }: PropsWithChildren) {
   const { locale } = useLoaderData<typeof loader>();
   const { i18n } = useAppTranslation();
-  const navigation = useNavigation();
-  const isLoadingPage = navigation.state === 'loading';
 
   useChangeLanguage(locale);
 
@@ -129,18 +56,60 @@ const Root = () => {
         <Links />
       </head>
       <body className="h-screen w-full">
-        <LoadingOverlay isLoading={isLoadingPage} />
-        <QueryClientProvider client={queryClient}>
-          <Outlet />
-          <ReactQueryDevtools
-            initialIsOpen={false}
-            buttonPosition="top-right"
-          />
-          <Toaster />
-          <Scripts />
-        </QueryClientProvider>
+        {children}
+        <Scripts />
+        <ScrollRestoration />
       </body>
     </html>
+  );
+}
+
+export function ErrorBoundary() {
+  const error = useRouteError();
+
+  useEffect(() => {
+    // Log to service of some sort
+    console.error(error);
+  }, [error]);
+
+  return <PageError errorText="There was an app crash!" />;
+}
+
+export function HydrateFallback() {
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+  }, []);
+
+  return (
+    <>
+      <LoadingOverlay isLoading={isLoading} />
+      <Scripts />
+      <noscript>
+        Your browser does not support JavaScript or it is not enabled! Please
+        re-enable JavaScript in order to use this site.
+      </noscript>
+    </>
+  );
+}
+
+const Root = () => {
+  const { locale } = useLoaderData<typeof loader>();
+  const navigation = useNavigation();
+  const isLoadingPage = navigation.state === 'loading';
+
+  useChangeLanguage(locale);
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <LoadingOverlay isLoading={isLoadingPage} />
+      <Outlet />
+      <ReactQueryDevtools initialIsOpen={false} buttonPosition="top-right" />
+      <Toaster />
+      <Scripts />
+      <ScrollRestoration />
+    </QueryClientProvider>
   );
 };
 
