c2e478701e255e95e2e7b3733b8c12276623d261
seanmcquaid <sean.m.mcquaid@gmail.com>
2024-03-12 22:42:42 -0400

    fix layout up

--- src/templates/vite-remix-spa/app/root.tsx
+++ src/templates/vite-remix-spa/app/root.tsx
@@ -7,58 +7,18 @@ import {
   Scripts,
   useRouteError,
   useNavigation,
+  ScrollRestoration,
 } from '@remix-run/react';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { QueryClientProvider } from '@tanstack/react-query';
+import type { PropsWithChildren } from 'react';
 import { useEffect, useState } from 'react';
 import { Toaster } from './components/ui/Toaster';
 import queryClient from './services/queryClient';
 import PageError from './components/app/PageError';
-import useAppTranslation from './hooks/useAppTranslation';
 import LoadingOverlay from './components/ui/LoadingOverlay';
 
-export function ErrorBoundary() {
-  const error = useRouteError();
-  const { i18n } = useAppTranslation();
-
-  useEffect(() => {
-    // Log to service of some sort
-    console.error(error);
-  }, [error]);
-
-  return (
-    <html lang={i18n.language}>
-      <head>
-        <meta charSet="UTF-8" />
-        <meta name="description" content="Vite App" />
-        <meta name="viewport" content="width=device-width,initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-        <meta name="theme-color" content="#ffffff" />
-        <meta
-          httpEquiv="Cache-Control"
-          content="no-cache, no-store, must-revalidate"
-        />
-        <meta httpEquiv="Pragma" content="no-cache" />
-        <meta httpEquiv="Expires" content="0" />
-        <title>{'<%= projectName %>'}</title>
-        <Meta />
-        <Links />
-      </head>
-      <body>
-        <PageError errorText="There was an app crash!" />
-        <Scripts />
-      </body>
-    </html>
-  );
-}
-
-export function HydrateFallback() {
-  const [isLoading, setIsLoading] = useState(false);
-
-  useEffect(() => {
-    setIsLoading(true);
-  }, []);
-
+export function Layout({ children }: PropsWithChildren) {
   return (
     // eslint-disable-next-line jsx-a11y/html-has-lang
     <html className="h-screen w-full">
@@ -78,54 +38,55 @@ export function HydrateFallback() {
         <Meta />
         <Links />
       </head>
-      <body>
-        <LoadingOverlay isLoading={isLoading} />
+      <body className="h-screen w-full">
+        {children}
         <Scripts />
-        <noscript>
-          Your browser does not support JavaScript or it is not enabled! Please
-          re-enable JavaScript in order to use this site.
-        </noscript>
+        <ScrollRestoration />
       </body>
     </html>
   );
 }
 
+export function ErrorBoundary() {
+  const error = useRouteError();
+
+  useEffect(() => {
+    // Log to service of some sort
+    console.error(error);
+  }, [error]);
+
+  return <PageError errorText="There was an app crash!" />;
+}
+
+export function HydrateFallback() {
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+  }, []);
+
+  return (
+    <>
+      <LoadingOverlay isLoading={isLoading} />
+      <noscript>
+        Your browser does not support JavaScript or it is not enabled! Please
+        re-enable JavaScript in order to use this site.
+      </noscript>
+    </>
+  );
+}
+
 const Root = () => {
-  const { i18n } = useAppTranslation();
   const navigation = useNavigation();
   const isLoadingPage = navigation.state === 'loading';
 
   return (
-    <html lang={i18n.language} className="h-screen w-full">
-      <head>
-        <meta charSet="UTF-8" />
-        <meta name="description" content="Vite App" />
-        <meta name="viewport" content="width=device-width,initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-        <meta name="theme-color" content="#ffffff" />
-        <meta
-          httpEquiv="Cache-Control"
-          content="no-cache, no-store, must-revalidate"
-        />
-        <meta httpEquiv="Pragma" content="no-cache" />
-        <meta httpEquiv="Expires" content="0" />
-        <title>{'<%= projectName %>'}</title>
-        <Meta />
-        <Links />
-      </head>
-      <body className="h-screen w-full">
-        <LoadingOverlay isLoading={isLoadingPage} />
-        <QueryClientProvider client={queryClient}>
-          <Outlet />
-          <ReactQueryDevtools
-            initialIsOpen={false}
-            buttonPosition="top-right"
-          />
-          <Toaster />
-          <Scripts />
-        </QueryClientProvider>
-      </body>
-    </html>
+    <QueryClientProvider client={queryClient}>
+      <LoadingOverlay isLoading={isLoadingPage} />
+      <Outlet />
+      <ReactQueryDevtools initialIsOpen={false} buttonPosition="top-right" />
+      <Toaster />
+    </QueryClientProvider>
   );
 };
 
