fa54455561b19eff0cdc5d1e0e715b8a78446ac0
seanmcquaid <sean.m.mcquaid@gmail.com>
2024-04-01 16:35:07 -0400

    spa mode

--- src/templates/vite-remix-spa/playwright/integration/mock-api.spec.ts
+++ src/templates/vite-remix-spa/playwright/integration/mock-api.spec.ts
@@ -1,4 +1,4 @@
-import { test, expect } from '../extensions';
+import { test, expect } from './extensions';
 
 test('Tests against a mocked API in the browser', async ({ page }) => {
   await page.goto('http://localhost:3000/kitchen-sink');
