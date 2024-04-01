f7f85e31584201bb1629e7e2fae3d5d2c9733f55
seanmcquaid <sean.m.mcquaid@gmail.com>
2024-04-01 16:42:34 -0400

    configs

--- src/templates/vite-remix-spa/vite.config.ts
+++ src/templates/vite-remix-spa/vite.config.ts
@@ -62,7 +62,7 @@ export default defineConfig({
     globals: true,
     environment: 'jsdom',
     setupFiles: ['./app/utils/testing/setupTests.ts'],
-    exclude: ['e2e', 'node_modules'],
+    exclude: ['playwright', 'node_modules'],
     coverage: {
       provider: 'istanbul',
       reporter: ['lcov'],
