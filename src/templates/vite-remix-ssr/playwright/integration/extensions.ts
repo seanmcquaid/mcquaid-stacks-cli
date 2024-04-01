91ba709cdf7d9ce41a0d6e5c6fd5fe66ca774876
seanmcquaid <sean.m.mcquaid@gmail.com>
2024-04-01 16:40:44 -0400

    another fix up

--- src/templates/vite-remix-ssr/playwright/integration/extensions.ts
+++ src/templates/vite-remix-ssr/playwright/integration/extensions.ts
@@ -1,7 +1,7 @@
 import { test as base, expect } from '@playwright/test';
 import type { MockServiceWorker } from 'playwright-msw';
 import { createWorkerFixture } from 'playwright-msw';
-import handlers from '../mocks/handlers';
+import handlers from '../../mocks/handlers';
 
 const test = base.extend<{
   worker: MockServiceWorker;
