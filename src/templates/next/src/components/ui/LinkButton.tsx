1a3643dcef2e3fdf2b52c1622f1c634831a46824
seanmcquaid <sean.m.mcquaid@gmail.com>
2024-06-17 14:54:14 -0400

    fix linting

--- src/templates/next/src/components/ui/LinkButton.tsx
+++ src/templates/next/src/components/ui/LinkButton.tsx
@@ -1,9 +1,9 @@
 import type { VariantProps } from 'class-variance-authority';
 import type { ReactNode } from 'react';
 import type { LinkProps } from 'next/link';
+import Link from 'next/link';
 import { buttonVariants } from './Button';
 import { cn } from '@/utils/styles';
-import Link from 'next/link';
 
 type LinkButtonProps = LinkProps &
   VariantProps<typeof buttonVariants> & {
