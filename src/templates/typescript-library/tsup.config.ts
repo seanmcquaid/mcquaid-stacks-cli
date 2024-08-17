44bd2c7ec6492b89b5d4b5e0c383568365121cf4
seanmcquaid <sean.m.mcquaid@gmail.com>
2024-08-16 23:24:03 -0400

    changeset

--- src/templates/typescript-library/tsup.config.ts
+++ src/templates/typescript-library/tsup.config.ts
@@ -2,7 +2,8 @@ import { defineConfig } from 'tsup';
 
 export default defineConfig({
   entry: ['src/index.ts'],
-  format: ['esm'],
+  format: ['esm', 'cjs'],
   minify: true,
   dts: true,
+  clean: true,
 });
