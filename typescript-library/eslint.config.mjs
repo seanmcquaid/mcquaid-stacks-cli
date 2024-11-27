90c3280e9a138f214ec379d1bdaa196575b0d353
seanmcquaid <sean.m.mcquaid@gmail.com>
2024-11-27 17:56:24 -0500

    eslint 9

--- typescript-library/eslint.config.mjs
+++ typescript-library/eslint.config.mjs
@@ -1,20 +1,10 @@
 import globals from 'globals';
 import pluginJs from '@eslint/js';
 import tseslint from 'typescript-eslint';
-import { parser } from 'typescript-eslint';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 import vitest from '@vitest/eslint-plugin';
 
 export default [
-  {
-    languageOptions: {
-      globals: { ...globals.browser, ...globals.node },
-      parser,
-    },
-    rules: {
-      'prettier/prettier': ['error'],
-    },
-  },
   {
     files: ['src/**'],
     plugins: {
@@ -27,4 +17,24 @@ export default [
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   eslintPluginPrettierRecommended,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        ...globals.serviceworker,
+      },
+    },
+    rules: {
+      'no-shadow': 'off',
+      '@typescript-eslint/no-var-requires': 0,
+      '@typescript-eslint/no-shadow': 'error',
+      curly: ['warn', 'all'],
+      'prefer-const': 'warn',
+      'prettier/prettier': 'warn',
+      '@typescript-eslint/no-namespace': 'off',
+      '@typescript-eslint/consistent-type-definitions': ['error'],
+      '@typescript-eslint/consistent-type-imports': 'error',
+    },
+  },
 ];
