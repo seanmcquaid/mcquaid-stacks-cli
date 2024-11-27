8d72a26a7042114f7c764374b3f9549354118374
seanmcquaid <sean.m.mcquaid@gmail.com>
2024-11-27 17:58:25 -0500

    config and recommended

--- typescript-library/eslint.config.mjs
+++ typescript-library/eslint.config.mjs
@@ -1,6 +1,7 @@
 import globals from 'globals';
 import pluginJs from '@eslint/js';
 import tseslint from 'typescript-eslint';
+import eslintConfigPrettier from 'eslint-config-prettier';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 import vitest from '@vitest/eslint-plugin';
 
@@ -16,6 +17,7 @@ export default [
   },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
+  eslintConfigPrettier,
   eslintPluginPrettierRecommended,
   {
     languageOptions: {
