2af56842382efcfc2b9e11f1130c2f2967d1d03e
seanmcquaid <sean.m.mcquaid@gmail.com>
2024-12-05 20:13:23 -0500

    spa forward ref

--- react-router-v7-spa/app/components/ui/Button.tsx
+++ react-router-v7-spa/app/components/ui/Button.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import type * as React from 'react';
 import { Slot } from '@radix-ui/react-slot';
 import { cva, type VariantProps } from 'class-variance-authority';
 import { cn } from '@/utils/styles';
@@ -38,16 +38,19 @@ export interface ButtonProps
   asChild?: boolean;
 }
 
-export const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, size, asChild = false, ...props }, ref) => {
-    const Comp = asChild ? Slot : 'button';
-    return (
-      <Comp
-        className={cn(buttonVariants({ variant, size, className }))}
-        ref={ref}
-        {...props}
-      />
-    );
-  },
-);
+export const Button = ({
+  className,
+  variant,
+  size,
+  asChild = false,
+  ...props
+}: React.HTMLProps<HTMLButtonElement> & ButtonProps) => {
+  const Comp = asChild ? Slot : 'button';
+  return (
+    <Comp
+      className={cn(buttonVariants({ variant, size, className }))}
+      {...props}
+    />
+  );
+};
 Button.displayName = 'Button';
--- react-router-v7-spa/app/components/ui/LinkButton.tsx
+++ react-router-v7-spa/app/components/ui/LinkButton.tsx
@@ -1,9 +1,15 @@
-import { Link, type LinkProps } from 'react-router';
 import type { VariantProps } from 'class-variance-authority';
+import type { ReactNode } from 'react';
+import type { LinkProps } from 'next/link';
+import Link from 'next/link';
 import { buttonVariants } from './Button';
 import { cn } from '@/utils/styles';
 
-type LinkButtonProps = LinkProps & VariantProps<typeof buttonVariants>;
+type LinkButtonProps = LinkProps &
+  VariantProps<typeof buttonVariants> & {
+    children: ReactNode;
+    className?: string;
+  };
 
 const LinkButton = (props: LinkButtonProps) => (
   <Link
