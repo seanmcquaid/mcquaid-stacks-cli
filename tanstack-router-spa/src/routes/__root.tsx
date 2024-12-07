15bb7d71aca093f167ba8f88ab40c4c0c9146d2b
seanmcquaid <sean.m.mcquaid@gmail.com>
2024-12-06 22:33:19 -0500

    initial setup for tanstack router

---
+++ tanstack-router-spa/src/routes/__root.tsx
@@ -0,0 +1,39 @@
+import * as React from "react";
+import { Link, Outlet, createRootRouteWithContext } from "@tanstack/react-router";
+import { TanStackRouterDevtools } from "@tanstack/router-devtools";
+import { QueryClient } from "@tanstack/react-query";
+
+export const Route = createRootRouteWithContext<{
+  queryClient: QueryClient
+}>()({
+  component: RootComponent,
+});
+
+function RootComponent() {
+  return (
+    <>
+      <div className="p-2 flex gap-2 text-lg">
+        <Link
+          to="/"
+          activeProps={{
+            className: "font-bold",
+          }}
+          activeOptions={{ exact: true }}
+        >
+          Home
+        </Link>{" "}
+        <Link
+          to="/about"
+          activeProps={{
+            className: "font-bold",
+          }}
+        >
+          About
+        </Link>
+      </div>
+      <hr />
+      <Outlet />
+      <TanStackRouterDevtools position="bottom-right" />
+    </>
+  );
+}
