dfc23939231ec505f4d3cb94936a7f9f55b92d59
seanmcquaid <sean.m.mcquaid@gmail.com>
2024-12-07 13:06:11 -0500

    linting

--- tanstack-router-spa/src/routes/react-query/$id/index.lazy.tsx
+++ tanstack-router-spa/src/routes/react-query/$id/index.lazy.tsx
@@ -1,20 +1,22 @@
-import { createLazyFileRoute } from '@tanstack/react-router'
-import { useSuspenseQuery } from '@tanstack/react-query'
-import PageWrapper from '@/components/app/PageWrapper'
-import { getPostQueryOptions } from '@/services/queries/posts'
+import { createLazyFileRoute } from '@tanstack/react-router';
+import { useSuspenseQuery } from '@tanstack/react-query';
+import PageWrapper from '@/components/app/PageWrapper';
+import { getPostQueryOptions } from '@/services/queries/posts';
 
 const ReactQueryPostPage = () => {
-  const { id } = Route.useParams()
-  const { data, isLoading, isError } = useSuspenseQuery(getPostQueryOptions(id))
+  const { id } = Route.useParams();
+  const { data, isLoading, isError } = useSuspenseQuery(
+    getPostQueryOptions(id),
+  );
 
   return (
     <PageWrapper isLoading={isLoading} isError={isError}>
       <h1>{data?.title}</h1>
       <p>{data?.body}</p>
     </PageWrapper>
-  )
-}
+  );
+};
 
 export const Route = createLazyFileRoute('/react-query/$id/')({
   component: ReactQueryPostPage,
-})
+});
--- tanstack-router-spa/src/routes/react-query/index.lazy.tsx
+++ tanstack-router-spa/src/routes/react-query/index.lazy.tsx
@@ -1,37 +1,37 @@
-import { createLazyFileRoute } from '@tanstack/react-router'
+import { createLazyFileRoute } from '@tanstack/react-router';
 import {
   useMutation,
   useQueryClient,
   useSuspenseQuery,
-} from '@tanstack/react-query'
-import PageWrapper from '@/components/app/PageWrapper'
-import { Button } from '@/components/ui/Button'
-import LinkButton from '@/components/ui/LinkButton'
-import useAppTranslation from '@/hooks/useAppTranslation'
-import { getPostsQueryOptions, PostsQueryKeys } from '@/services/queries/posts'
-import { useToast } from '@/hooks/useToast'
-import postsService from '@/services/postsService'
+} from '@tanstack/react-query';
+import PageWrapper from '@/components/app/PageWrapper';
+import { Button } from '@/components/ui/Button';
+import LinkButton from '@/components/ui/LinkButton';
+import useAppTranslation from '@/hooks/useAppTranslation';
+import { getPostsQueryOptions, PostsQueryKeys } from '@/services/queries/posts';
+import { useToast } from '@/hooks/useToast';
+import postsService from '@/services/postsService';
 
 const ReactQueryPage = () => {
-  const { t } = useAppTranslation()
-  const { data, isLoading, isError } = useSuspenseQuery(getPostsQueryOptions())
-  const queryClient = useQueryClient()
-  const { toast } = useToast()
+  const { t } = useAppTranslation();
+  const { data, isLoading, isError } = useSuspenseQuery(getPostsQueryOptions());
+  const queryClient = useQueryClient();
+  const { toast } = useToast();
   const { mutate: deletePost, isPending: deletePostLoading } = useMutation({
     mutationFn: async (id: string) => postsService.deletePost(id),
     onSuccess: () => {
       queryClient.invalidateQueries({
         queryKey: [PostsQueryKeys.GET_POSTS],
-      })
-      toast({ title: 'I got deleted' })
+      });
+      toast({ title: 'I got deleted' });
     },
-  })
+  });
 
   return (
     <PageWrapper isLoading={isLoading} isError={isError}>
       <h1>{t('ReactQueryPage.title')}</h1>
       <ul className="grid grid-cols-2">
-        {data?.map((post) => (
+        {data?.map(post => (
           <li key={post.id} className="mt-4 flex items-center">
             {post.title.substring(0, 5)}
             <Button
@@ -54,9 +54,9 @@ const ReactQueryPage = () => {
         ))}
       </ul>
     </PageWrapper>
-  )
-}
+  );
+};
 
 export const Route = createLazyFileRoute('/react-query/')({
   component: ReactQueryPage,
-})
+});
