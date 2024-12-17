04b84836a4e4976f704ed9612da907e348461de6
seanmcquaid <sean.m.mcquaid@gmail.com>
2024-12-17 17:41:37 -0500

    oops

--- typescript-library/src/useFunPackage/useFunPackage.test.ts
+++ typescript-library/src/useFunPackage/useFunPackage.test.ts
@@ -2,6 +2,6 @@ import { useFunPackage } from './index';
 
 describe('useFunPackage', () => {
   it('returns a negative number when a positive is provided', () => {
-    expect(useFunPackage({ num: 100 })).toEqual(-100);
+    expect(useFunPackage({ num: 100 })).toEqual(100);
   });
 });
